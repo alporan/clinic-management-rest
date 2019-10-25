040e2b2bf29026de2f430c605bb91ef0b1111cbc
alporan <alporan@windowslive.com>
2019-10-25 18:01:28 +0300

    Initial commmit.

---
+++ src/main/java/com/orana/clinicmanagementrest/ClinicManagementRestApplication.java
@@ -0,0 +1,13 @@
+package com.orana.clinicmanagementrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagementRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orana/clinicmanagementrest/ClinicManagementRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orana.clinicmanagementrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
