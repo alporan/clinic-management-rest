73033ea6ef7d9155a94782aa11cfe50d02091bcf
alporan <alporan@windowslive.com>
2019-10-28 16:33:55 +0300

    Test commit.

---
+++ src/main/java/com/orana/clinicmanagementrest/model/Contact.java
@@ -0,0 +1,22 @@
+package com.orana.clinicmanagementrest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+}
